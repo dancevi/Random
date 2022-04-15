bb2ea4e2567bd940ea77455d9025934a0b0e963f
dancevi <94169229+dancevi@users.noreply.github.com>
2022-04-15 18:02:21 +0300

    problem solved - sorting to be added

--- judge.py
+++ judge.py
@@ -20,5 +20,18 @@ while command != "no more time":
         if user not in my_dict[contest].keys():
             my_dict[contest][user] = points
 
+            if user not in total_points_dict:
+                total_points_dict[user] = points
+            else:
+                total_points_dict[user] += points
+        else:
+            if points > my_dict[contest][user]:
+                my_dict[contest][user] = points
+                total_points_dict[user] = points
+            elif points == my_dict[contest][user]:
+                total_points_dict[user] += points
 
     command = input()
+
+print(my_dict)
+print(total_points_dict)
